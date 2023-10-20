a39aa84e1a10a363d2baddf5cdcf64315363af91
TheMalachite <eliasgheeraert@gmail.com>
2023-10-20 22:42:45 +0530

    rosemary: Build few radio interfaces
    Change-Id: I2db1106018f3468f636194e3da65fef5bd86e88e

--- device.mk
+++ device.mk
@@ -400,6 +400,17 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio@1.1 \
+    android.hardware.radio@1.2 \
+    android.hardware.radio@1.3 \
+    android.hardware.radio@1.4 \
+    android.hardware.radio@1.5 \
+    android.hardware.radio@1.6 \
+    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio.config@1.2 \
+    android.hardware.radio.config@1.3 \
     android.hardware.radio@1.0.vendor \
     android.hardware.radio@1.1.vendor \
     android.hardware.radio@1.2.vendor \
